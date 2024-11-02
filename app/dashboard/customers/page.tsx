c4e18e1eaac41be97a2912f9773d1dce5cc6ad75
Sankar Mahadevan V <sankarsmd100@gmail.com>
2024-11-02 08:01:43 +0530

    done changes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Customers = () => {
+  return <div>Customer Page</div>;
+};
+
+export default Customers;
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
 
 export default function SideNav() {
   return (
@@ -14,7 +14,7 @@ export default function SideNav() {
           <AcmeLogo />
         </div>
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex grow flex-row overflow-auto justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
